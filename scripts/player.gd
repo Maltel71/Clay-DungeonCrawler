11c51ba4d89884e3fe0d5c22ae7cf9328c947b92
kwarnberg <kristoffer.warnberg@gmail.com>
2025-11-03 21:55:42 +0100

    Add placeholder QnD camera and movement

---
+++ scripts/player.gd
@@ -0,0 +1,35 @@
+extends CharacterBody3D
+
+@export var movement_speed = 3.0
+const JUMP_VELOCITY = 4.5
+
+@onready var pivot: Node3D = $CameraPoint
+@export var mouse_sensitivity: float = 0.25
+
+func _ready() -> void:
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+
+func _input(event: InputEvent) -> void:
+	if event is InputEventMouseMotion:
+		rotate_y(deg_to_rad(-event.relative.x * mouse_sensitivity))
+		pivot.rotate_x(deg_to_rad(-event.relative.y * mouse_sensitivity))
+		pivot.rotation.x = clamp(pivot.rotation.x, deg_to_rad(-90), deg_to_rad(45))
+
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	#if not is_on_floor():
+		#velocity += get_gravity() * delta
+
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	var input_dir := Input.get_vector("left", "right", "up", "down")
+	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	if direction:
+		velocity.x = direction.x * movement_speed
+		velocity.z = direction.z * movement_speed
+	else:
+		velocity.x = move_toward(velocity.x, 0, movement_speed)
+		velocity.z = move_toward(velocity.z, 0, movement_speed)
+
+	move_and_slide()
